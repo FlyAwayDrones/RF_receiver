a7d89284922b1f2c908ea6c0aabbe27f078c5e08
Felipe Roscoche <fe.r@outlook.com>
2017-03-15 01:17:41 -0300

    Minimized code and stablished hex communication

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # RF_receiver
-This repository contains all that is needed to receive our transmitter and control de drone.
+This repository contains all that is needed to receive our transmitter and control the drone.
--- RF_receiver.ino
+++ RF_receiver.ino
@@ -10,10 +10,16 @@ RF24 radio(9,10);
 //Create channels for communication
 const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
 
+//Variables needed
+char receivedControls[6] = "aaaa;";
+char nadave[6] = "CUCU;";
+
+
 void setup(void)
 {
   //Start serial communication and print info
   Serial.begin(57600);
+  printf_begin();
   Serial.println("-- RF_receiver - Drone Board --");
 
   //Start radio communication, open writing and reading pipes
@@ -29,67 +35,35 @@ void setup(void)
   //Listen to Radio and print details about it.
   radio.startListening();
   radio.printDetails();
+  
 }
 
 void loop(void)
 {
-  
-    // if there is data ready
+    //check if there is data available
     if ( radio.available() )
     {
       // Dump the payloads until we've gotten everything
-      unsigned long got_time;
+      
       bool done = false;
-      while (!done)
-      {
-        // Fetch the payload, and see if this was the last one.
-        done = radio.read( &got_time, sizeof(unsigned long) );
-
-        // Spew it
-        printf("Got payload %lu...",got_time);
-
-	// Delay just a little bit to let the other unit
-	// make the transition to receiver
-	delay(20);
-      }
-
+            
+      while(!done)
+        done=radio.read( &receivedControls, sizeof(receivedControls));
+      
+      Serial.print("Posicoes: ");
+      Serial.print(receivedControls);
+      
+      
       // First, stop listening so we can talk
       radio.stopListening();
 
+      
       // Send the final one back.
-      radio.write( &got_time, sizeof(unsigned long) );
-      printf("Sent response.\n\r");
-
+      Serial.println(" ... Enviando resposta.");
+      radio.write( &receivedControls, sizeof(nadave) );
+      
       // Now, resume listening so we catch the next packets.
       radio.startListening();
     }
-  }
-
-  //
-  // Change roles
-  //
-
-  if ( Serial.available() )
-  {
-    char c = toupper(Serial.read());
-    if ( c == 'T' && role == role_pong_back )
-    {
-      printf("*** CHANGING TO TRANSMIT ROLE -- PRESS 'R' TO SWITCH BACK\n\r");
-
-      // Become the primary transmitter (ping out)
-      role = role_ping_out;
-      radio.openWritingPipe(pipes[0]);
-      radio.openReadingPipe(1,pipes[1]);
-    }
-    else if ( c == 'R' && role == role_ping_out )
-    {
-      printf("*** CHANGING TO RECEIVE ROLE -- PRESS 'T' TO SWITCH BACK\n\r");
-      
-      // Become the primary receiver (pong back)
-      role = role_pong_back;
-      radio.openWritingPipe(pipes[1]);
-      radio.openReadingPipe(1,pipes[0]);
-    }
-  }
+  
 }
-// vim:cin:ai:sts=2 sw=2 ft=cpp
