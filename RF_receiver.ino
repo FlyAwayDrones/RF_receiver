cac482770380a7b9c97a577ffdb706d10811598d
Felipe Roscoche <fe.r@outlook.com>
2017-03-15 15:04:11 -0300

    Interpret j1y and move servo

--- RF_receiver.ino
+++ RF_receiver.ino
@@ -1,3 +1,4 @@
+#include <Servo.h>
 #include <SPI.h>
 #include "nRF24L01.h"
 #include "RF24.h"
@@ -12,8 +13,9 @@ const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
 
 //Variables needed
 char receivedControls[6] = "aaaa;";
-char nadave[6] = "CUCU;";
+int j1x,j1y,j2x,j2y;
 
+Servo myservo;
 
 void setup(void)
 {
@@ -35,6 +37,31 @@ void setup(void)
   //Listen to Radio and print details about it.
   radio.startListening();
   radio.printDetails();
+  myservo.attach(3);
+  pinMode(4,OUTPUT);
+  digitalWrite(4,HIGH);
+}
+int hextodecimal(char c){
+  switch (c){
+    case 48: return 0; break;
+    case 49: return 1; break;
+    case 50: return 2; break;
+    case 51: return 3; break;
+    case 52: return 4; break;
+    case 53: return 5; break;
+    case 54: return 6; break;
+    case 55: return 7; break;
+    case 56: return 8; break;
+    case 57: return 9; break;
+    case 97: return 10; break;
+    case 98: return 11; break;
+    case 99: return 12; break;
+    case 100: return 13; break;
+    case 101: return 14; break;
+    case 102: return 15; break;
+    default: return -1; break;
+    
+  }
   
 }
 
@@ -52,15 +79,21 @@ void loop(void)
       
       Serial.print("Posicoes: ");
       Serial.print(receivedControls);
-      
-      
+      j1y=hextodecimal(receivedControls[1]);
+      Serial.print("Valor: ");
+      Serial.print(j1y);
+      j1y=map(j1y,0,15,0,180);
+      j1y=180-j1y;
+      myservo.write(j1y);
+      Serial.print("Servo: ");
+      Serial.print(j1y);
       // First, stop listening so we can talk
       radio.stopListening();
 
       
       // Send the final one back.
       Serial.println(" ... Enviando resposta.");
-      radio.write( &receivedControls, sizeof(nadave) );
+      radio.write( &receivedControls, sizeof(receivedControls) );
       
       // Now, resume listening so we catch the next packets.
       radio.startListening();
