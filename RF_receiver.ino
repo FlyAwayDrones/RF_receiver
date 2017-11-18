0e8700058bd2dafdac84006ac6e6d1a0e3a6204d
levymateus <levymateusmacedo@gmail.com>
2017-11-18 17:45:02 -0200

    dev-helper

--- RF_receiver.ino
+++ RF_receiver.ino
@@ -1,3 +1,4 @@
+#include <flyaway_protocol.h>
 #include <Servo.h>
 #include <SPI.h>
 #include "nRF24L01.h"
@@ -15,7 +16,21 @@ const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
 char receivedControls[6] = "aaaa;";
 int j1x,j1y,j2x,j2y;
 
-Servo myservo;
+//Servo myservo;
+Servo servo1;
+Servo servo2;
+Servo servo3;
+Servo servo4;
+
+void z_axis_handle(int16_t z){
+  z=map(z, 0, 15, 0, 180);
+  z=180-z;
+  Serial.println(z);
+  servo1.write(z);
+  servo2.write(z);
+  servo3.write(z);
+  servo4.write(z);
+}
 
 void setup(void)
 {
@@ -37,10 +52,12 @@ void setup(void)
   //Listen to Radio and print details about it.
   radio.startListening();
   radio.printDetails();
-  myservo.attach(3);
+  //myservo.attach(3);
+  servo1.attach(3);
   pinMode(4,OUTPUT);
   digitalWrite(4,HIGH);
 }
+
 int hextodecimal(char c){
   switch (c){
     case 48: return 0; break;
@@ -67,15 +84,16 @@ int hextodecimal(char c){
 
 void loop(void)
 {
+    char buff[128];
     //check if there is data available
     if ( radio.available() )
     {
       // Dump the payloads until we've gotten everything
       
-      bool done = false;
+      /*bool done = false;
             
-      while(!done)
-        done=radio.read( &receivedControls, sizeof(receivedControls));
+      //while(!done)
+        radio.read( &receivedControls, sizeof(receivedControls));
       
       Serial.print("Posicoes: ");
       Serial.print(receivedControls);
@@ -90,10 +108,28 @@ void loop(void)
       // First, stop listening so we can talk
       radio.stopListening();
 
-      
       // Send the final one back.
       Serial.println(" ... Enviando resposta.");
-      radio.write( &receivedControls, sizeof(receivedControls) );
+      radio.write( &receivedControls, sizeof(receivedControls) );*/
+      fad_manual_control manual_control;
+      radio.read(&buff, sizeof(buff));
+
+      switch(get_message_id(buff))
+      {
+        case FLYAWAY_MANUAL_CONTROL:
+            decode_manual_control_frame(buff, &manual_control);
+    
+            Serial.println(get_message_id(buff));
+            Serial.println(manual_control.x);
+            Serial.println(manual_control.y);
+            Serial.println(manual_control.z);
+              
+            //z_axis_handle(manual_control.z);
+          break;
+        
+        default: 
+          break;
+      }
       
       // Now, resume listening so we catch the next packets.
       radio.startListening();
